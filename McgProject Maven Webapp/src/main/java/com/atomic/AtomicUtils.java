7a77da8b0d86d40f1673f6ffb42d646ec5f8eb2a
hehaifeng <hehaifeng@roobo.com>
2018-04-19 18:23:40 +0800

    gitignore111223

--- McgProject Maven Webapp/src/main/java/com/atomic/AtomicUtils.java
+++ McgProject Maven Webapp/src/main/java/com/atomic/AtomicUtils.java
@@ -1,8 +1,19 @@
 package com.atomic;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by macg11 on 2018/4/18.
  */
 public class AtomicUtils {
+
+    public static void main(String[] args){
+        String aa="[3,4,5]";
+        List a = Arrays.asList(aa);
+        if(a.contains(3)){
+            System.out.println("包含");
+        }
+    }
 
 }
